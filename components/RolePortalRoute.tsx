dc8a72e12a262e5ed2b4438f74198242c859427d
Sunmughan Swamy <sunmughaN@Gmail.com>
2026-09-06 20:07:34 +0530

    Use compiled differentiated workspace renderer

--- components/RolePortalRoute.tsx
+++ components/RolePortalRoute.tsx
@@ -1,4 +1,4 @@
 'use client';
-import DiverseRoleScreen from './DiverseRoleScreen';
+import ScreenWorkspace from './ScreenWorkspace';
 import {PortalShell} from './PortalShell';
-export default function RolePortalRoute({role,slug}:{role:string;slug:string}){return <PortalShell role={role} slug={slug}><DiverseRoleScreen role={role} slug={slug}/></PortalShell>}
+export default function RolePortalRoute({role,slug}:{role:string;slug:string}){return <PortalShell role={role} slug={slug}><ScreenWorkspace role={role} slug={slug}/></PortalShell>}
